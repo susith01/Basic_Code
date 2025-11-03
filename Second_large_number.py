0669f34fb1e9f92d4ecbb3a3211b4e5eb16ec83b
susith01 <susihravichandran@gmail.com>
2025-11-03 19:07:32 +0530

    Find the second large number in array

---
+++ Second_large_number.py
@@ -0,0 +1,11 @@
+class solution:
+    def __init__(self,number):
+        self.number=number
+
+    def sort_number(self):
+        self.number.sort()
+        return self.number
+
+a=solution([1,13,2,4,15,12])
+result=a.sort_number()
+print(result)        
