e22a2bafa00a9ac623f00e68c8a2889ccd20db61
susith01 <susihravichandran@gmail.com>
2025-11-08 11:35:11 +0530

    Count the number and divide the total number.

---
+++ Count_Digits.py
@@ -0,0 +1,17 @@
+class Solution:
+    def evenlyDivides(self, n):
+      
+        count = 0              
+
+        for i in str(n):      
+          d = int(i)           
+          if d != 0:          
+            if n % d == 0:
+             count += 1   
+
+        return count
+
+obj=Solution()
+n=120
+result=obj.evenlyDivides(n)
+print(result)   
