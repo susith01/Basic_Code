67412ee5e58abc9b7fd6acc51ee2665903b5fde7
susith01 <susihravichandran@gmail.com>
2025-11-11 23:35:18 +0530

    Remove the extra spaces

--- Last_Digit.py
+++ Last_Digit.py
@@ -1,16 +1,9 @@
 class PowerFinder:
-
     def __init__(self, a, b):  
-   
         self.a = int(a)
         self.b = int(b)
-
     def find_last_digit(self):
-     
         d = self.a ** self.b
-   
         return str(d)[-1]
-
 obj = PowerFinder("3", "10")
-
 print(obj.find_last_digit())
